7db11accacce04c2f00ecf99940f9f9231db61d2
hvsamanyousif-prog <hvsamanyousif@gmail.com>
2026-09-15 07:51:12 +0200

    v65 wire mobility route into shared public build

--- scripts/build_public_pilot.py
+++ scripts/build_public_pilot.py
@@ -45,6 +45,7 @@ ADULT_EXTRA_COSTS_PATH = "client/adult-extra-costs-guidance.js"
 CHILD_MAINTENANCE_PATH = "client/child-maintenance-guidance.js"
 OLDER_HOME_SUPPORT_PATH = "client/older-home-support-guidance.js"
 DISABILITY_HOME_SUPPORT_PATH = "client/disability-home-support-guidance.js"
+MOBILITY_TRANSPORT_PATH = "client/mobility-transport-guidance.js"
 QUICK_LEARNING_PATH = "client/quick-help-feedback.js"
 QUICK_GUIDANCE_PATH = "client/quick-help-guidance.js"
 HOUSING_GUIDANCE_PATH = "client/housing-adaptation-guidance.js"
@@ -78,6 +79,7 @@ SHELL_RUNTIME_PATHS = (
     CHILD_MAINTENANCE_PATH,
     OLDER_HOME_SUPPORT_PATH,
     DISABILITY_HOME_SUPPORT_PATH,
+    MOBILITY_TRANSPORT_PATH,
 )
 QUICK_RUNTIME_PATHS = (QUICK_LEARNING_PATH, QUICK_GUIDANCE_PATH, HOUSING_GUIDANCE_PATH, DENTAL_67_GUIDANCE_PATH)
 SCRIPT_PATHS = (
@@ -110,6 +112,7 @@ SCRIPT_PATHS = (
     CHILD_MAINTENANCE_PATH,
     OLDER_HOME_SUPPORT_PATH,
     DISABILITY_HOME_SUPPORT_PATH,
+    MOBILITY_TRANSPORT_PATH,
 )
 PROFILE_PATH = "config/public_pilot_capabilities.json"
 PERSON_PILOT_PATH = "person-pilot.html"
